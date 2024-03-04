8ba41b6be9f594cbe9450bfe38f232087e25a7b1
Jadon Forman <jdone4man@gmail.com>
2024-03-03 23:43:23 -0600

    created gameObjects + player

--- rpg game/game.py
+++ rpg game/game.py
@@ -1,5 +1,7 @@
 import pygbag.aio as asyncio
 import pygame
+from gameObject import GameObject
+from player import Player
 
 class Game: 
     # Initialize the game
@@ -14,25 +16,31 @@ class Game:
         self.bg_color = (255, 255, 255)
         self.bg_layer = pygame.Surface(self.screen.get_size())
         self.bg_layer.fill(self.bg_color)
-        bg_image = pygame.image.load('assets/background.png')
-        self.background = pygame.transform.scale(bg_image, (self.screen.get_size()))
+        self.background = GameObject(0, 0, self.width, self.height, 'assets/background.png')
 
         # Treasure
-        treasure_image = pygame.image.load('assets/treasure.png')
-        self.treasure = pygame.transform.scale(treasure_image, (50, 50))
+        self.treasure = GameObject(375, 50, 50, 50, 'assets/treasure.png')
+
+        # Player
+        self.player = Player(375, 700, 50, 50, 'assets/player.png', 10)
 
     # Draw the game objects
     def draw_objects(self):
         # Draw the background
         self.screen.blit(self.bg_layer, (0, 0))
-        self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.treasure, (375, 50))
+        self.screen.blit(self.background.image, (self.background.x, self.background.y))
+        self.screen.blit(self.treasure.image, (self.treasure.x, self.treasure.y))
+        self.screen.blit(self.player.image, (self.player.x, self.player.y))
                     
         # Update the display
         pygame.display.flip()
             
     # Game loop
     async def run_game_loop(self):
+        
+        player_direction = 0
+
+        # Main game loop
         running = True
         while running:
             
@@ -41,8 +49,17 @@ class Game:
             for event in events:
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        player_direction = -1
+                    elif event.key == pygame.K_DOWN:
+                        player_direction = 1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        player_direction = 0
             
             # Execute logic
+            self.player.move(player_direction)
             self.draw_objects()  # Draw the game objects
             await asyncio.sleep(0)  # Yield control for smooth execution
 
